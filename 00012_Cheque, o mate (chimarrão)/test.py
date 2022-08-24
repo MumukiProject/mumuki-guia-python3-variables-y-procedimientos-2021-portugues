779c451741f596c4c652ac9bf4da774b42394de9
rocio@mumuki.org <rocio@mumuki.org>
2022-08-24 21:01:14 +0000

    Mumuki Export on 2022-08-24 21:01:14 +0000

--- 00012_Cheque, o mate (chimarrão)/test.py
+++ 00012_Cheque, o mate (chimarrão)/test.py
@@ -1,17 +1,17 @@
   
   def test_cebar_mate_disminuye_en_30_ml_el_agua_del_termo(self):
-    global agua_do_chimarrao
-    agua_do_chimarrao = 1000
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 1000
     fazer_chimarrao()
-    self.assertEqual(agua_do_chimarrao, 970)
+    self.assertEqual(agua_da_garrafa_termica, 970)
 
   def test_cebar_3_mates_disminuye_en_90_ml_el_agua_del_termo(self):
-    global agua_do_chimarrao
-    agua_do_chimarrao = 1000
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 1000
     fazer_chimarrao()
     fazer_chimarrao()
     fazer_chimarrao()
-    self.assertEqual(agua_do_chimarrao, 910)
+    self.assertEqual(agua_da_garrafa_termica, 910)
     
   def test_cebar_mate_aumenta_en_30_ml_el_agua_del_mate(self):
     global agua_do_chimarrao
