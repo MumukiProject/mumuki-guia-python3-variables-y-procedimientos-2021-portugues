577080c8bc28371fb0023ca23f6002b5d1681926
rocio@mumuki.org <rocio@mumuki.org>
2022-08-24 16:54:13 +0000

    Mumuki Export on 2022-08-24 16:54:13 +0000

---
+++ 00008_Deixe o último apagar a luz!/description.md
@@ -0,0 +1,14 @@
+Agora que estamos familiarizados com os procedimentos, podemos modelar casos de alternância usando `not`. Por exemplo, acender e apagar uma luz :bulb::
+
+```python
+luz_acesa = False
+
+def apertar_interruptor():
+  global luz_acesa
+  luz_acesa = not luz_acesa
+```
+
+Agora é a sua vez!
+
+
+> Defina o procedimento `usar_ziper` para que possamos abrir e fechar nossa mochila. :school_satchel:
--- 00008_Deixe o último apagar a luz!/test.py
+++ 00008_Deixe o último apagar a luz!/test.py
@@ -1,17 +1,17 @@
   
   def test_si_la_mochila_estaba_abierta_al_ejecutar_usar_cierre_pasa_a_estar_cerrada(self):
-    usar_cierre()
+    usar_ziper()
     self.assertFalse(mochila_abierta)
   
   
   def test_si_la_mochila_estaba_cerrada_al_ejecutar_usar_cierre_pasa_a_estar_abierta(self):
     global mochila_abierta
     mochila_abierta = False
-    usar_cierre()
+    usar_ziper()
     self.assertTrue(mochila_abierta)
   
   
   def test_si_la_mochila_estaba_abierta_al_ejecutar_usar_cierre_dos_veces_sigue_estando_abierta(self):
-    usar_cierre()
-    usar_cierre()
+    usar_ziper()
+    usar_ziper()
     self.assertTrue(mochila_abierta)
