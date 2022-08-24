9be6683849b8d4386e9b29777d5ff89e0d81631f
rocio@mumuki.org <rocio@mumuki.org>
2022-08-24 20:45:21 +0000

    Mumuki Export on 2022-08-24 20:45:21 +0000

--- 00010_A água esfriou/description.md
+++ 00010_A água esfriou/description.md
@@ -2,5 +2,5 @@ Não importa quão boa seja a garrafa térmica, às vezes a água fica fria. :co
 
 > Defina os procedimentos:
 
-> * `esvaziar_garrafa` que tira toda a `agua_da_garrafa`, ou seja, deixa em 0 a variável;
+> * `esvaziar_garrafa` que tira toda a `agua_da_garrafa_termica`, ou seja, deixa em 0 a variável;
 > * `encher_garrafa` que volta a colocar nossa variável em 1000.
--- 00010_A água esfriou/test.py
+++ 00010_A água esfriou/test.py
@@ -1,13 +1,13 @@
   
   def test_vaciar_termo_deja_en_0_el_agua_del_termo(self):
-    global agua_da_garrafa
-    agua_da_garrafa = 1000
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 1000
     esvaziar_garrafa()
     self.assertEqual(agua_da_garrafa, 0)
 
 
   def test_llenar_termo_deja_en_1000_el_agua_del_termo(self):
-    global agua_da_garrafa
-    agua_da_garrafa = 80
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 80
     encher_garrafa()
     self.assertEqual(agua_da_garrafa, 1000)
