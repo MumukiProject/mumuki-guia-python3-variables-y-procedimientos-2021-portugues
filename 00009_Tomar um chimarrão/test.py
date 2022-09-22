f79540b6f8f6e56207da35a78c0cc908f5ae86a6
rocio@mumuki.org <rocio@mumuki.org>
2022-09-22 16:19:15 +0000

    Mumuki Export on 2022-09-22 16:19:15 +0000

--- 00009_Tomar um chimarrão/test.py
+++ 00009_Tomar um chimarrão/test.py
@@ -1,12 +1,12 @@
   
-  def test_cebar_mate_disminuye_en_30_ml_el_agua_del_termo(self):
+  def test_fazer_chimarrão_diminui_para_30_ml_a_água_da_garrafa_térmica(self):
     global agua_da_garrafa_termica
     agua_da_garrafa_termica = 1000
     fazer_chimarrao()
     self.assertEqual(agua_da_garrafa_termica, 970)
 
 
-  def test_cebar_3_mates_disminuye_en_90_ml_el_agua_del_termo(self):
+  def test_fazer_3_chimarrões_diminui_para_90_ml_a_água_da_garrafa_térmica(self):
     global agua_da_garrafa_termica
     agua_da_garrafa_termica = 1000
     fazer_chimarrao()
