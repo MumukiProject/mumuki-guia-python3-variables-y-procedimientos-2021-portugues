3bc1d8522efb3def5be9d0b6e2fb305ef0031fcb
rocio@mumuki.org <rocio@mumuki.org>
2022-09-22 16:29:28 +0000

    Mumuki Export on 2022-09-22 16:29:28 +0000

--- 00011_Acho que não vou tomar mais/test.py
+++ 00011_Acho que não vou tomar mais/test.py
@@ -1,12 +1,12 @@
   
-  def test_cargar_termo_con_200_como_argumento_aumenta_en_200_el_agua_del_termo(self):
+  def test_encher_garrafa_térmica_com_200_como_argumento_aumenta_para_200_a_água_da_garrafa_térmica(self):
     global agua_da_garrafa_termica
     agua_da_garrafa_termica = 600
     colocar_agua_na_garrafa(200)
     self.assertEqual(agua_da_garrafa_termica, 800)
 
 
-  def test_cargar_termo_con_500_como_argumento_aumenta_en_500_el_agua_del_termo(self):
+  def test_encher_garrafa_térmica_com_500_como_argumento_aumenta_para_500_a_água_da_garrafa_térmica(self):
     global agua_da_garrafa_termica
     agua_da_garrafa_termica = 200
     colocar_agua_na_garrafa(500)
