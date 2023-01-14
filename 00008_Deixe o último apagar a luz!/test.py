ebe5e3ff950fee793448f866f928a5a52c3a6a90
franco@mumuki.org <franco@mumuki.org>
2023-01-14 03:02:57 +0000

    Mumuki Export on 2023-01-14 03:02:57 +0000

--- 00008_Deixe o último apagar a luz!/test.py
+++ 00008_Deixe o último apagar a luz!/test.py
@@ -1,17 +1,17 @@
   
   def test_se_a_mochila_estava_aberta_ao_executar_usar_zíper_passa_a_estar_fechada(self):
     usar_ziper()
-    self.assertFalse(mochila_abierta)
+    self.assertFalse(mochila_aberta)
   
   
   def test_se_a_mochila_estava_fechada_ao_executar_usar_zíper_passa_a_estar_aberta(self):
-    global mochila_abierta
-    mochila_abierta = False
+    global mochila_aberta
+    mochila_aberta = False
     usar_ziper()
-    self.assertTrue(mochila_abierta)
+    self.assertTrue(mochila_aberta)
   
   
   def test_se_a_mochila_estava_aberta_ao_executar_usar_zíper_duas_vezes_continua_estando_aberta(self):
     usar_ziper()
     usar_ziper()
-    self.assertTrue(mochila_abierta)
+    self.assertTrue(mochila_aberta)
