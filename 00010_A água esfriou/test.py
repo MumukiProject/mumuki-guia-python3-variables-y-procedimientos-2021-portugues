9144f46eb7d8d2feb3334c15807ce37e1d2e0275
franco@mumuki.org <franco@mumuki.org>
2023-01-20 22:17:28 +0000

    Mumuki Export on 2023-01-20 22:17:28 +0000

--- 00010_A água esfriou/test.py
+++ 00010_A água esfriou/test.py
@@ -1,9 +1,24 @@
-  
-  def test_esvaziar_a_garrafa_térmica_deixa_em_0_a_água_da_garrafa_térmica(self):
+
+  def test_esvaziar_a_garrafa_térmica_deixa_em_0_a_água_da_garrafa_térmica_se_tivesse_1000(self):
     global agua_da_garrafa_termica
     agua_da_garrafa_termica = 1000
     esvaziar_garrafa()
     self.assertEqual(agua_da_garrafa_termica, 0)
+
+  
+  def test_esvaziar_a_garrafa_térmica_deixa_em_0_a_água_da_garrafa_térmica_se_tivesse_500(self):
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 500
+    esvaziar_garrafa()
+    self.assertEqual(agua_da_garrafa_termica, 0)
+
+  
+  def test_esvaziar_a_garrafa_térmica_deixa_em_0_a_água_da_garrafa_térmica_se_tivesse_200(self):
+    global agua_da_garrafa_termica
+    agua_da_garrafa_termica = 200
+    esvaziar_garrafa()
+    self.assertEqual(agua_da_garrafa_termica, 0)
+
 
 
   def test_encher_garrafa_térmica_deixa_em_1000_a_água_da_garrafa_térmica(self):
