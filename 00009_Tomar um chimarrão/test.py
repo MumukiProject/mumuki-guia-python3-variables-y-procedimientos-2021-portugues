47551e1aa9ff6ad3c97d1ff72e51d6548ddadb3b
franco@mumuki.org <franco@mumuki.org>
2023-01-20 22:11:44 +0000

    Mumuki Export on 2023-01-20 22:11:44 +0000

--- 00009_Tomar um chimarrão/test.py
+++ 00009_Tomar um chimarrão/test.py
@@ -1,6 +1,6 @@
   
   def test_a_princípio_a_água_da_garrafa_térmica_e_1000(self):
-    self.assertEqual(agua_del_termo, 1000, "a água na garrafa térmica deve ser inicializada em 1000")
+    self.assertEqual(agua_da_garrafa_termica, 1000, "a água na garrafa térmica deve ser inicializada em 1000")
 
   def test_fazer_chimarrão_diminui_para_30_ml_a_água_da_garrafa_térmica(self):
     global agua_da_garrafa_termica
